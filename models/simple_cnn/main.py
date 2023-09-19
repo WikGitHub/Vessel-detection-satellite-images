5972977eb8f960a871eb0b36d9ddcda3f41b43b6
Wiktoria Kasprzak <kasprzak_w@yahoo.com>
2023-09-19 20:06:29 +0100

    init of simple cnn

--- models/simple_cnn/main.py
+++ models/simple_cnn/main.py
@@ -1,5 +1,33 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
 from models.base_model.main import ImageClassificationBase
 
 
-class SimpleCnn(ImageClassificationBase):
-    pass
+class BaseCnn(ImageClassificationBase):
+    """
+    Implementation of a simple CNN architecture with a few convolutional and connected layers. This model is going to be used as a base to compare other models to.
+    """
+
+    def __init__(self):
+        """
+        Initialise the model by setting up the layers.
+        """
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(16 * 16 * 16, 64)
+        self.fc2 = nn.Linear(64, 2)
+
+    def forward(self, data):
+        """
+        Forward pass through the network.
+        :param data: input data to the network.
+        :return: output of the network.
+        """
+        data = F.relu(self.conv1(data))
+        data = self.pool(data)
+        data = data.view(-1, 16 * 40 * 40)
+        data = F.relu(self.fc1(data))
+        data = self.fc2(data)
+        return data
